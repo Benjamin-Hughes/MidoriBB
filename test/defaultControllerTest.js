11b7857537ce79fea61c30ae96fa353c7331ec19
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2018-01-10 01:23:30 +0000

    fix issue in app.js preventing environment variables from loading. At some inital test data in tests

--- test/defaultControllerTest.js
+++ test/defaultControllerTest.js
@@ -1,15 +1,28 @@
+process.env.NODE_ENV = 'test';
 const chai = require("chai");
 const expect = chai.expect;
 const chaiHttp = require("chai-http");
+const models = require("../models");
 
 chai.use(chaiHttp);
 
-describe("Default Controller", () => {
-    describe("Default", () => {
-        it("Should return the index page", (done) => {
+describe("Default Controller", function() {
+    
+    before(function() {
+        models.Topic.findOrCreate({ where: { title: 'Introduce yourself', description: 'New here? Come say hi' }})
+        .spread((topic, created) => {
+            console.log(topic.get({
+                plain: true
+            }));
+            console.log(created);
+        });
+    });
+
+    describe("Default", function() {
+        it("Should return the index page", function(done) {
             chai.request("http://localhost:3000")
             .get("/")
-            .end((err, res) => {
+            .end(function(err, res) {
                 expect(res).to.have.status(200);
                 done();
             });
